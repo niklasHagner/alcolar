0e70ca2968665deeb7cb1b58607cd16b2998fa8a
Niklas Hagner <niklas.hagner@outlook.com>
2014-10-05 18:17:24 +0200

    Version 0.1
    - support for API search with all avaliable filters
    - Angular app with search controller and factory
    - GUI with product presentation and search bar featuring  alcohol and
    price min/max filters
    - basic "add favourite" functionality

---
+++ drinkCategory.js
@@ -0,0 +1,543 @@
+var drinkCategory = [
+      {
+        "id": "1",
+        "name": "cognac"
+      },
+      {
+        "id": "2",
+        "name": "liquor"
+      },
+      {
+        "id": "3",
+        "name": "whisky"
+      },
+      {
+        "id": "4",
+        "name": "malt"
+      },
+      {
+        "id": "5",
+        "name": "rum"
+      },
+      {
+        "id": "6",
+        "name": "beer"
+      },
+      {
+        "id": "7",
+        "name": "ale"
+      },
+      {
+        "id": "8",
+        "name": "stout"
+      },
+      {
+        "id": "9",
+        "name": "grappa"
+      },
+      {
+        "id": "10",
+        "name": "gin"
+      },
+      {
+        "id": "11",
+        "name": "tequila"
+      },
+      {
+        "id": "12",
+        "name": "brandy"
+      },
+      {
+        "id": "13",
+        "name": "calvados"
+      },
+      {
+        "id": "14",
+        "name": "armegnac"
+      },
+      {
+        "id": "15",
+        "name": "sake"
+      },
+      {
+        "id": "16",
+        "name": "cider"
+      },
+      {
+        "id": "17",
+        "name": "sweet"
+      },
+      {
+        "id": "18",
+        "name": "bourbon"
+      },
+      {
+        "id": "19",
+        "name": "lager"
+      },
+      {
+        "id": "20",
+        "name": "wine"
+      },
+      {
+        "id": "21",
+        "name": "dry"
+      },
+      {
+        "id": "22",
+        "name": "rose"
+      },
+      {
+        "id": "23",
+        "name": "red-wine"
+      },
+      {
+        "id": "24",
+        "name": "sparkling"
+      },
+      {
+        "id": "25",
+        "name": "white-wine"
+      },
+      {
+        "id": "26",
+        "name": "non-alcohol"
+      },
+      {
+        "id": "27",
+        "name": "water"
+      },
+      {
+        "id": "28",
+        "name": "mead"
+      },
+      {
+        "id": "29",
+        "name": "jenever"
+      },
+      {
+        "id": "30",
+        "name": "mulled-wine"
+      },
+      {
+        "id": "31",
+        "name": "punsch"
+      },
+      {
+        "id": "32",
+        "name": "bitter"
+      },
+      {
+        "id": "33",
+        "name": "apertif"
+      },
+      {
+        "id": "34",
+        "name": "sherry"
+      },
+      {
+        "id": "35",
+        "name": "vermouth"
+      }
+    ]; 
+               
+    $scope.countryCodes = [
+
+      {
+        "id": "1",
+        "name": "Frankrike"
+      },
+      {
+        "id": "2",
+        "name": "Sverige"
+      },
+      {
+        "id": "3",
+        "name": "Polen"
+      },
+      {
+        "id": "4",
+        "name": "USA"
+      },
+      {
+        "id": "5",
+        "name": "Storbritannien"
+      },
+      {
+        "id": "6",
+        "name": "Belgien"
+      },
+      {
+        "id": "7",
+        "name": "Nederländerna"
+      },
+      {
+        "id": "8",
+        "name": "Trinidad"
+      },
+      {
+        "id": "9",
+        "name": "Schweiz"
+      },
+      {
+        "id": "10",
+        "name": "Internationellt märke"
+      },
+      {
+        "id": "11",
+        "name": "Italien"
+      },
+      {
+        "id": "12",
+        "name": "Mexiko"
+      },
+      {
+        "id": "13",
+        "name": "Danmark"
+      },
+      {
+        "id": "14",
+        "name": "Chile"
+      },
+      {
+        "id": "15",
+        "name": "Spanien"
+      },
+      {
+        "id": "16",
+        "name": "Österrike"
+      },
+      {
+        "id": "17",
+        "name": "Barbados"
+      },
+      {
+        "id": "18",
+        "name": "Norge"
+      },
+      {
+        "id": "19",
+        "name": "Japan"
+      },
+      {
+        "id": "20",
+        "name": "Förpackning"
+      },
+      {
+        "id": "21",
+        "name": "Irland"
+      },
+      {
+        "id": "22",
+        "name": "Peru"
+      },
+      {
+        "id": "23",
+        "name": "Finland"
+      },
+      {
+        "id": "24",
+        "name": "Tyskland"
+      },
+      {
+        "id": "25",
+        "name": "Australien"
+      },
+      {
+        "id": "26",
+        "name": "Kanada"
+      },
+      {
+        "id": "27",
+        "name": "Sydafrika"
+      },
+      {
+        "id": "28",
+        "name": "Tjeckien"
+      },
+      {
+        "id": "29",
+        "name": "Nya Zeeland"
+      },
+      {
+        "id": "30",
+        "name": "Ungern"
+      },
+      {
+        "id": "31",
+        "name": "Israel"
+      },
+      {
+        "id": "32",
+        "name": "Portugal"
+      },
+      {
+        "id": "33",
+        "name": "Colombia"
+      },
+      {
+        "id": "34",
+        "name": "Grekland"
+      },
+      {
+        "id": "35",
+        "name": "Thailand"
+      },
+      {
+        "id": "36",
+        "name": "Kroatien"
+      },
+      {
+        "id": "37",
+        "name": "Turkiet"
+      },
+      {
+        "id": "38",
+        "name": "Jamaica"
+      },
+      {
+        "id": "39",
+        "name": "Ryssland"
+      },
+      {
+        "id": "40",
+        "name": "Argentina"
+      },
+      {
+        "id": "41",
+        "name": "Slovakien"
+      },
+      {
+        "id": "42",
+        "name": "Cypern"
+      },
+      {
+        "id": "43",
+        "name": "Övriga ursprung"
+      },
+      {
+        "id": "44",
+        "name": "Bulgarien"
+      },
+      {
+        "id": "45",
+        "name": "Rumänien"
+      },
+      {
+        "id": "46",
+        "name": "Kuba"
+      },
+      {
+        "id": "47",
+        "name": "Haiti"
+      },
+      {
+        "id": "48",
+        "name": "Venezuela"
+      },
+      {
+        "id": "49",
+        "name": "Dominikanska Republiken"
+      },
+      {
+        "id": "50",
+        "name": "Indien"
+      },
+      {
+        "id": "51",
+        "name": "Guatemala"
+      },
+      {
+        "id": "52",
+        "name": "Indonesien"
+      },
+      {
+        "id": "53",
+        "name": "Panama"
+      },
+      {
+        "id": "54",
+        "name": "Paraguay"
+      },
+      {
+        "id": "55",
+        "name": "Guyana"
+      },
+      {
+        "id": "56",
+        "name": "Varierande ursprung"
+      },
+      {
+        "id": "57",
+        "name": "Serbien"
+      },
+      {
+        "id": "58",
+        "name": "Brasilien"
+      },
+      {
+        "id": "59",
+        "name": "Georgien"
+      },
+      {
+        "id": "60",
+        "name": "Libanon"
+      },
+      {
+        "id": "61",
+        "name": "Bahamas"
+      },
+      {
+        "id": "62",
+        "name": "Slovenien"
+      },
+      {
+        "id": "63",
+        "name": "Okänt ursprung"
+      },
+      {
+        "id": "64",
+        "name": "Namibia"
+      },
+      {
+        "id": "65",
+        "name": "Makedonien (F.Y.R.O.M)"
+      },
+      {
+        "id": "66",
+        "name": "Tunisien"
+      },
+      {
+        "id": "67",
+        "name": "Uruguay"
+      },
+      {
+        "id": "68",
+        "name": "Montenegro"
+      },
+      {
+        "id": "69",
+        "name": "Sydkorea"
+      },
+      {
+        "id": "70",
+        "name": "Bosnien-Hercegovina"
+      },
+      {
+        "id": "71",
+        "name": "Luxemburg"
+      },
+      {
+        "id": "72",
+        "name": "Malta"
+      },
+      {
+        "id": "73",
+        "name": "Armenien"
+      },
+      {
+        "id": "74",
+        "name": "Moldavien"
+      },
+      {
+        "id": "75",
+        "name": "Lettland"
+      },
+      {
+        "id": "76",
+        "name": "St Lucia"
+      },
+      {
+        "id": "77",
+        "name": "Ukraina"
+      },
+      {
+        "id": "78",
+        "name": "Island"
+      },
+      {
+        "id": "79",
+        "name": "Grenada"
+      },
+      {
+        "id": "80",
+        "name": "Filippinerna"
+      },
+      {
+        "id": "81",
+        "name": "Surinam"
+      },
+      {
+        "id": "82",
+        "name": "Litauen"
+      },
+      {
+        "id": "83",
+        "name": "Fiji"
+      },
+      {
+        "id": "84",
+        "name": "Nicaragua"
+      },
+      {
+        "id": "85",
+        "name": "Estland"
+      },
+      {
+        "id": "86",
+        "name": "Mauritius"
+      },
+      {
+        "id": "87",
+        "name": "Costa Rica"
+      },
+      {
+        "id": "88",
+        "name": "Kosovo"
+      },
+      {
+        "id": "89",
+        "name": "Singapore"
+      },
+      {
+        "id": "90",
+        "name": "Marocko"
+      },
+      {
+        "id": "91",
+        "name": "Kenya"
+      },
+      {
+        "id": "92",
+        "name": "Uganda"
+      },
+      {
+        "id": "93",
+        "name": "Vietnam"
+      },
+      {
+        "id": "94",
+        "name": "Nigeria"
+      },
+      {
+        "id": "95",
+        "name": "Folkrepubliken Kina"
+      },
+      {
+        "id": "96",
+        "name": "Etiopien"
+      },
+      {
+        "id": "97",
+        "name": "Sri Lanka"
+      },
+      {
+        "id": "98",
+        "name": "Republiken Togo"
+      },
+      {
+        "id": "99",
+        "name": "Tanzania"
+      }
+
+   ];
---
+++ script.js
@@ -0,0 +1,200 @@
+// Code goes here
+
+var app = angular.module('myApp', []);
+
+app.factory('systembolagetSearch', function ($q, $http) {
+
+    return {
+
+        getDataFromSystembolaget: function (filterSettings) {
+            var deferred = $q.defer();
+            search = "";
+            var filters = filterSettings.getFilterString();
+            var apiUrl = "http://systemetapi.se/product?";
+            var preventCorsProblems = "&callback=?";
+            var url = apiUrl + filters + preventCorsProblems;
+            
+            $.ajax({
+			  url: url,
+			  async: true, cache:true,
+			  dataType: 'json',
+			  success: function (data) {
+					var products = data;
+					deferred.resolve(products);
+			  },
+			  error: function(jqXHR, textStatus, errorString) {
+				console.log("error calling API." +"\n  Returned with Status: " + textStatus + "\n  Error: " + errorString);
+			  }
+			});
+            
+            return deferred.promise;
+        }
+    };
+});
+
+
+
+app.controller('SystembolagetSearchController', ['$scope', 'systembolagetSearch', '$timeout', '$rootScope', function ($scope, systembolagetSearch, $timeout, $rootScope) {
+
+  
+    $scope.search = "";
+    $scope.products = [];
+		$scope.editFavouritesView = true;
+		$scope.searchView = true;
+		$scope.favouriteProducts = [];
+		$scope.currentProduct = $scope.favouriteProducts[0];
+		$scope.currentProductIndex = 0;
+				
+		$scope.removeProduct = function(index) {
+			$scope.favouriteProducts.splice(index, 1);
+		};
+    
+    $scope.performSearch = function (value) {
+            $scope.products = [];
+            var promise = systembolagetSearch.getDataFromSystembolaget($scope.filterSettings);
+
+            promise.then(function (productsData) {
+                // $scope.products = data;
+                angular.forEach(productsData, function (product) {
+                    
+                    var searchProduct = product;
+                    searchProduct.name = product.name;
+                    searchProduct.article_id = product.article_id;
+                    searchProduct.img = "http:\/\/www.systembolaget.se\/imagevaultfiles\/id_11184\/cf_1915\/" + product.article_id + ".jpg";
+                    $scope.products.push(searchProduct);
+                });
+                 
+                //$scope.getImages($scope.products);
+            })
+           
+
+       
+    };
+    
+                
+            
+	 $scope.getImages = function(list) {
+	
+         var errorCounter = 0;
+		 for(var i=0;i<15; i++) {
+             var id = list[i].product_number.substring(0,5);
+             //id = list[i].article_id < 6 ? list[i].article_id.substring(0,4) : list[i].article_id.substring(0,5) ;
+			
+             
+             for (var attempt = 3; attempt < 8; attempt++) {
+             
+                 id = list[i].product_number.substring(0,attempt);
+                 var url = "http://systembolagetapi.se/?id=" + id + "&callback=?"; //avoids cross-domain-problems
+                     console.log("attempt : " + attempt + " @ " + id);
+                 
+                $.ajax({
+                  url: url,
+                  async: true, cache:true,
+                  dataType: 'json',
+                  success: function (data) {
+                        console.log("found image ");
+                        /*var imgSrc = decodeURI(data.image);
+                        list[this.songArrayIndex]["img"] = imgSrc;
+                        $scope.$apply(); */
+                  },
+                  error: function(jqXHR, textStatus, errorString) {
+                    //console.log("error calling spotify:s search API.");
+                      errorCounter++;
+                  }
+                });
+             }
+		
+		 }
+	 }
+            
+    $scope.filterSettings = { 
+        drinkCategory : { key: "tag", value: "6" },
+        
+        options_orderby : ["name", "price", "price_per_liter", "alcohol", "apk"],
+        orderby : { key: "order_by", value: "alcohol" }, 
+        order : { key: "order", value: "ASC" },
+      
+        
+       
+        country : { key: "country", value: "1" },
+      
+        name : { key: "name", value: "Laphroaig" },
+        
+        maxVolume : { key: "max_volume", value: 0.33 },
+        limit : { key: "limit", value: 50 },
+        offset : { key: "offset", value: 0 },
+        
+        alcoholMin : { key: "alcohol_from", value: 0.035 },
+        alcoholMax : { key: "alcohol_to", value: 0.15 },
+        minPrice : { key: "price_from", value: 20 },
+        maxPrice : { key: "price_to", value: 70 },
+        minPricePerLiter : { key: "price_per_liter_from", value: 10 },
+        maxPricePerLiter : { key: "price_per_liter_to", value: 1000 },
+        apkMin : { key: "apk_from", value: 0.9 },
+        apkMax : { key: "apk_to", value: 6 },
+        
+        
+        minDate : { key: "start_date_from", value: "2014-03-31" },
+        maxDate : { key: "start_date_to", value: "2014-05-01" },
+        minYear : { key: "year_from", value: "2006" },
+        maxYear : { key: "year_to", value: "2014" }
+    
+
+		
+	};
+    $scope.filterSettings.filterArray = [  
+        
+        $scope.filterSettings.drinkCategory ,
+        //$scope.filterSettings.maxVolume,
+        $scope.filterSettings.alcoholMin ,
+        //$scope.filterSettings.alcoholMax ,
+        //$scope.filterSettings.apkMin ,
+        //$scope.filterSettings.apkMax ,
+        //$scope.filterSettings.country ,
+        //$scope.filterSettings.limit , 
+        //$scope.filterSettings.name
+        //$scope.filterSettings.offset ,
+        $scope.filterSettings.minPrice ,
+        $scope.filterSettings.maxPrice
+        //$scope.filterSettings.minPricePerLiter ,  
+        //$scope.filterSettings.maxPricePerLiter ,
+        //$scope.filterSettings.minDate ,
+        //$scope.filterSettings.maxDate ,
+        //$scope.filterSettings.minYear ,
+        //$scope.filterSettings.maxYear
+    ];
+    $scope.filterSettings.getFilterString = function() {
+        var filterStringArray = [];
+        for (var i = 0; i< this.filterArray.length; i++) {
+            filterStringArray.push($scope.filterSettings.filterArray[i].key + "=" + this.filterArray[i].value);
+        }
+        filterStringArray.push($scope.filterSettings.orderby.key + "=" + $scope.filterSettings.orderby.value);
+        filterStringArray.push($scope.filterSettings.order.key + "=" + $scope.filterSettings.order.value);
+        var filterString = filterStringArray.join("&");
+        return filterString;
+    };
+        
+    
+    $scope.$watch(function combinedWatch() {
+        return {
+            search: $scope.filterSettings.name
+        };
+    }, function (value) {
+        $scope.performSearch(value);
+    }, true);
+    
+    $scope.addProduct = function (product) {
+      this.favouriteProducts.push(product);
+			this.products.splice(this.products.indexOf(product), 1);
+    };
+
+    $scope.toggleFavouritesView = function() {
+        $scope.editFavouritesView = !$scope.editFavouritesView;
+    };
+
+    $scope.toggleSearchView = function() {
+        $scope.searchView = !$scope.searchView;
+    };
+
+	
+}]);
